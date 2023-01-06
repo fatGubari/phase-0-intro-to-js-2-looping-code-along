8d30e4e8753b0e4c76a1574bae1ef91b042a2588
fatima abdullah <fatimajubary@gmail.com>
2023-01-06 16:20:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+  
+  console.log(writeCards(names,"surprise"))
+function writeCards(names,event){  
+    let name = [];
+    for (let i = 0; i < names.length; i++) {
+        name.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }   
+    return name
+}
+
+countDown(4)
+function countDown(n){
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+      }
+}
